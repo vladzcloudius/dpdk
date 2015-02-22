701a655a097cf7eabd7801303a0649644756d39d
Vlad Zolotarov <vladz@cloudius-systems.com>
2015-02-22 17:03:52 +0200

    ixgbe: Enable LRO Rx offload capability
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- lib/librte_pmd_ixgbe/ixgbe_ethdev.c
+++ lib/librte_pmd_ixgbe/ixgbe_ethdev.c
@@ -2008,7 +2008,8 @@ ixgbe_dev_info_get(struct rte_eth_dev *dev, struct rte_eth_dev_info *dev_info)
 		DEV_RX_OFFLOAD_VLAN_STRIP |
 		DEV_RX_OFFLOAD_IPV4_CKSUM |
 		DEV_RX_OFFLOAD_UDP_CKSUM  |
-		DEV_RX_OFFLOAD_TCP_CKSUM;
+		DEV_RX_OFFLOAD_TCP_CKSUM  |
+        DEV_RX_OFFLOAD_TCP_LRO;
 	dev_info->tx_offload_capa =
 		DEV_TX_OFFLOAD_VLAN_INSERT |
 		DEV_TX_OFFLOAD_IPV4_CKSUM  |
