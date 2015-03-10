dc2fac4e6204ff7fda3aecbf2b65f386e5568d6d
Vlad Zolotarov <vladz@cloudius-systems.com>
2015-03-10 18:17:50 +0200

    ixgbe: Fixed rx_bulk_alloc_allowed and rx_vec_allowed initialization
       - Don't set them to FALSE in rte_eth_dev_stop() flow - the following
         rte_eth_dev_start() will need them.
       - Reset them to TRUE in rte_eth_dev_configure() and not in a probe() flow.
         This will ensure the proper behaviour if port is re-configured.
    
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- lib/librte_pmd_ixgbe/ixgbe_ethdev.c
+++ lib/librte_pmd_ixgbe/ixgbe_ethdev.c
@@ -772,13 +772,6 @@ eth_ixgbe_dev_init(__attribute__((unused)) struct eth_driver *eth_drv,
 	hw->hw_addr = (void *)pci_dev->mem_resource[0].addr;
 	hw->allow_unsupported_sfp = 1;
 
-	/*
-	 * Initialize to TRUE. If any of Rx queues doesn't meet the bulk
-	 * allocation or vector Rx preconditions we will reset it.
-	 */
-	hw->rx_bulk_alloc_allowed = true;
-	hw->rx_vec_allowed = true;
-
 	/* Initialize the shared code (base driver) */
 #ifdef RTE_NIC_BYPASS
 	diag = ixgbe_bypass_init_shared_code(hw);
@@ -1441,11 +1434,20 @@ ixgbe_dev_configure(struct rte_eth_dev *dev)
 {
 	struct ixgbe_interrupt *intr =
 		IXGBE_DEV_PRIVATE_TO_INTR(dev->data->dev_private);
+	struct ixgbe_hw *hw =
+		IXGBE_DEV_PRIVATE_TO_HW(dev->data->dev_private);
 
 	PMD_INIT_FUNC_TRACE();
 
 	/* set flag to update link status after init */
 	intr->flags |= IXGBE_FLAG_NEED_LINK_UPDATE;
+
+	/*
+	 * Initialize to TRUE. If any of Rx queues doesn't meet the bulk
+	 * allocation or vector Rx preconditions we will reset it.
+	 */
+	hw->rx_bulk_alloc_allowed = true;
+	hw->rx_vec_allowed = true;
 
 	return 0;
 }
@@ -1649,8 +1651,6 @@ ixgbe_dev_stop(struct rte_eth_dev *dev)
 	/* Clear stored conf */
 	dev->data->scattered_rx = 0;
 	dev->data->lro = 0;
-	hw->rx_bulk_alloc_allowed = false;
-	hw->rx_vec_allowed = false;
 
 	/* Clear recorded link status */
 	memset(&link, 0, sizeof(link));
